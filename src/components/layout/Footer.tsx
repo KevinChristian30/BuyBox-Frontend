5d970306140a87d4c9e879e944d3f894ef2df9a1
Kevin Christian <kdotchrist30@gmail.com>
2024-02-24 01:58:26 +0700

    refactor: increased footer height

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-16 grid place-items-center px-4 text-center">
+    <div className="h-32 grid place-items-center px-4 text-center">
       BuyBox ©{new Date().getFullYear()} Created by FreestyleDevs (Josafat
       Anderson Yonain, Kevin Christian, Rico Gunawan)
     </div>
