850d3fcfebc1d5c0b8278625855b09808c5cf8dd
Kevin Christian <kdotchrist30@gmail.com>
2024-03-05 21:27:58 +0700

    feat: added navigate to create product pagea

--- src/app/(main)/store/page.tsx
+++ src/app/(main)/store/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import Spacer, { SpacerDirection } from "@/components/commons/Spacer";
+import { ProductOutlined } from "@ant-design/icons";
+import { Button, Typography } from "antd";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-  return <div>Store</div>;
+  return (
+    <div className="my-16 mx-24">
+      <Typography.Title>Your Store</Typography.Title>
+      <Spacer direction={SpacerDirection.VERTICAL} space={40} />
+      <div className="">
+        <Link href={"/products/create"}>
+          <Button type="primary" size="large" icon={<ProductOutlined />}>
+            Add New Product
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
